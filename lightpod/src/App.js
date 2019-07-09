31aaa324ce9e7b463855288b11df047ddb74c9c0
lovisgod <olifedayo94@gmail.com>
2019-07-09 10:35:08 +0100

    FT/redirect by login button template

--- lightpod/src/App.js
+++ lightpod/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
@@ -8,13 +7,28 @@ import Home from './components/Home';
 import Signup from './components/Signup';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        loginstatus : null
+    }
+}
+
+handleLoginCheck = loginstat => {
+  console.log("handle login check called" + loginstat);
+  this.setState({loginstatus : loginstat});
+ 
+}
+
   render() { 
     return (
       <BrowserRouter>
       <div className="App">
-        <Navbar title="LightPod" logintitle="Login" signuptitle="Sign Up"/>
+        <Navbar title="LightPod" logintitle="Login" signuptitle="Sign Up" loginstat={this.state.loginstatus}/>
         <Route exact path="/" render={()=> <Home title="Podcast List"/>} />
-        <Route exact path="/login" render={()=> <Login title="Login"/>} />
+        <Route exact path="/login" render={()=> <Login title="Login" onsubmmit={this.handleLoginCheck}/>} />
         <Route exact path="/signup" render={()=> <Signup title="Sign Up"/>} />
       </div>
       </BrowserRouter>
--- lightpod/src/components/Login.jsx
+++ lightpod/src/components/Login.jsx
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
+import Home from './Home';
 
 class Login extends Component {
+  
+    
+   
+    handleLogin = event => {
+        event.preventDefault();
+        this.props.onsubmmit("Ayooluwa");
+        this.props.history.push('/');
+        // <Route exact to='/' render = {() => <Home />} />
+    }
     
     render() { 
         return ( 
             <div className="login-container">
                 <div className="login-header">{this.props.title}</div>
                 <form>
-                    <div class="form-group">
+                    <div className="form-group">
                         <input type="email" className="form-control" placeholder="Enter email"></input>
                         {/* <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> */}
                     </div>
-                    <div class="form-group">
-                         <input type="password" class="form-control"  placeholder="Password"></input>
+                    <div className="form-group">
+                         <input type="password" className="form-control"  placeholder="Password"></input>
                     </div>
                      
-                    <button type="submit" class="btn btn-primary login-submit">Submit</button>
+                    <button type="submit" className="btn btn-primary login-submit" onClick={this.handleLogin}>Submit</button>
                 </form>
             </div>
          );
     }
 }
+
+
  
-export default Login;
+export default withRouter(Login);
