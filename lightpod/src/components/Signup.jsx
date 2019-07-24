974bc4506ebddc10401929996ed60afab74b597a
Ruth Agbasimalo <aruth71@gmail.com>
2019-07-24 09:33:55 +0100

    FT/ SignUp (Added Basic Event Handlers)

--- lightpod/src/components/Signup.jsx
+++ lightpod/src/components/Signup.jsx
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isSignedIn : false,
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+
+  onNameChange = (event) => {
+    this.setState({username: event.target.value});
+  }
+
+  onEmailChange = (event) => {
+    this.setState({email: event.target.value});
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({password: event.target.value});
+  }
+
+  onConfirmPasswordChange = (event) => {
+    this.setState({confirmPassword: event.target.value});
+  }
+
+  
   render() { 
     return ( 
       <div className="login-container">
         <div className="login-header">{this.props.title}</div>
         <form>
+        <div className="form-group">
+            <input type="text" className="form-control"  placeholder="Enter Username" onChange={this.onNameChange}></input>
+          </div> 
           <div className="form-group">
-            <input type="text" className="form-control" placeholder="Enter Username"></input>
-            <input type="email" className="form-control" placeholder="Enter Email"></input>
+            <input type="email" className="form-control" placeholder="Enter Email" onChange={this.onEmailChange}></input>
           </div>
           <div className="form-group">
-            <input type="password" class="form-control"  placeholder="Password"></input>
+            <input type="password" className="form-control"  placeholder="Password" onChange={this.onPasswordChange}></input>
+          </div> 
+          <div className="form-group">
+            <input type="password" className="form-control"  placeholder="Confirm Password" onChange={this.onConfirmPasswordChange}></input>
           </div>   
-          <button type="submit" class="btn btn-primary login-submit">Submit</button>
+          <button type="submit" className="btn btn-primary login-submit">Submit</button>
         </form>
       </div>
     );
