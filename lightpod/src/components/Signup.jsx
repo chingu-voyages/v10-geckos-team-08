211d213a94d8965892c669a54c3fc4ca1db93a27
Ruth Agbasimalo <aruth71@gmail.com>
2019-07-24 19:25:01 +0100

    FT/Sign Up

--- lightpod/src/components/Signup.jsx
+++ lightpod/src/components/Signup.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { app, auth } from '../utils/app';
 
 class Signup extends Component {
   constructor(props) {
@@ -7,8 +8,7 @@ class Signup extends Component {
       isSignedIn : false,
       username: '',
       email: '',
-      password: '',
-      confirmPassword: ''
+      password: ''
     }
   }
 
@@ -24,33 +24,35 @@ class Signup extends Component {
     this.setState({password: event.target.value});
   }
 
-  onConfirmPasswordChange = (event) => {
-    this.setState({confirmPassword: event.target.value});
+
+  onSubmitSignIn = async event => {
+    const { username, email, password } = this.state;
+    event.preventDefault();
+    console.log(this.state);
+    await auth.createUserWithEmailAndPassword(email, password)
+      .catch(error => console.error(error.message))
   }
 
-  
   render() { 
-    return ( 
+    const { username, email, password } = this.state;
+    return (
       <div className="login-container">
         <div className="login-header">{this.props.title}</div>
-        <form>
-        <div className="form-group">
-            <input type="text" className="form-control"  placeholder="Enter Username" onChange={this.onNameChange}></input>
-          </div> 
+        <form onSubmit={this.onSubmitSignIn}>
           <div className="form-group">
-            <input type="email" className="form-control" placeholder="Enter Email" onChange={this.onEmailChange}></input>
+            <input type="text" className="form-control"  placeholder="Enter Username" value = {username} onChange={this.onNameChange}></input>
           </div>
           <div className="form-group">
-            <input type="password" className="form-control"  placeholder="Password" onChange={this.onPasswordChange}></input>
-          </div> 
+            <input type="email" className="form-control" placeholder="Enter Email" value = {email} onChange={this.onEmailChange}></input>
+          </div>
           <div className="form-group">
-            <input type="password" className="form-control"  placeholder="Confirm Password" onChange={this.onConfirmPasswordChange}></input>
-          </div>   
+            <input type="password" className="form-control"  placeholder="Password" value = {password} onChange={this.onPasswordChange}></input>
+          </div>
           <button type="submit" className="btn btn-primary login-submit">Submit</button>
         </form>
       </div>
     );
   }
 }
- 
+
 export default Signup;
